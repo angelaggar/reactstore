c5fe017ca3d786e2b718e7155936fcd9c269b56f
angelagar <ggarcia.angelad@gmail.com>
2024-03-30 00:35:30 -0600

    home changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,25 @@
-export default function Home () {
-  return <h1>Home</h1>
+import { Link, Outlet } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <main>
+      <header className='flex justify-center items-center h-36 bg-[rgb(8,126,164)] text-white text-7xl gluten gap-5'>
+        <img
+          src='https://logos-world.net/wp-content/uploads/2023/08/React-Symbol.png'
+          alt=''
+          className='h-36'
+        />
+        <h1 >Store</h1>
+      </header>
+      <nav className='bg-sky-300 p-3 text-xl font-semibold flex gap-20 justify-center'>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/products'}>Products</Link>
+        {/* <Link to={'/dos'}>dos</Link> */}
+        <Link to={'/login'}>Login</Link>
+      </nav>
+      <section>
+        <Outlet />
+      </section>
+    </main>
+  )
 }
