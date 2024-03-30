9767a87d5930f007ecb3afd6be0e60df9a080fdb
angelagar <ggarcia.angelad@gmail.com>
2024-03-30 00:38:09 -0600

    others

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,51 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/LoginRHF'
 import Products from './pages/Products'
 import Todos from './pages/Todos'
+import ProductOnShow from './components/ProductOnShow'
 
-
-// se crean las rutas con el createBrowserRouter, 
+// se crean las rutas con el createBrowserRouter,
 //que recibe como parametro un array de objetos de las diferentes rutas
 const routes = createBrowserRouter([
   {
-    path:'/',
-    element: <Home />
+    path: '/',
+    element: <Home />,
+    children:[
+      {
+        path:'/uno',
+        element: <h1>Uno</h1>
+      },
+      {
+        path:'/dos',
+        element: <h1>Dos</h1>
+      },
+      {
+        path:'/tres',
+        element: <h1>Tres</h1>
+      },
+      {
+        path: '/products',
+        element: <Products />
+      },
+    ]
   },
   {
     path: '/login',
-    element: <Login/>
+    element: <Login />
   },
   {
-    path: '/products',
-    element: <Products/>,
-    // children: [
-    //   {
-    //     path: '/id',
-    //     element: <h1>Product</h1>
-    //   }
-    // ]
+    path: '/products/:id',
+    element: <ProductOnShow />
   },
   {
     path: '/todos',
-    element: <Todos/>
+    element: <Todos />
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router= {routes} />
+  <RouterProvider router={routes} />
 )
